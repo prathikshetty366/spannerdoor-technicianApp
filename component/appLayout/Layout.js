d6303457ab4fa48abfcdcf31b79a1443ce54db09
Pratheek shetty <pratheek.shetty@simpleenergy.in>
2024-01-23 19:48:26 +0530

    added inactivity hook

--- component/appLayout/Layout.js
+++ component/appLayout/Layout.js
@@ -2,26 +2,28 @@ import styles from "./layout.module.scss";
 import Image from "next/image";
 import { useRouter } from "next/router";
 
-export const AppLayout = ({ children,isLoggedIn}) => {
-    const router=useRouter()
-    const handleLogout=()=>{
-        localStorage.setItem("isLoggedIn","")
+export const AppLayout = ({ children, isLoggedIn }) => {
+    const router = useRouter()
+    const handleLogout = () => {
+        localStorage.setItem("isLoggedIn", "")
         router.push("/")
     }
     return (
         <div className={styles.layoutWrapper}>
             <div className={styles.navbar}>
-                <div className={styles.navheader} style={{justifyContent:!isLoggedIn?"center":""}}>
+                <div className={styles.navheader} style={{ justifyContent: !isLoggedIn ? "center" : "" }}>
                     <Image
-                    src="/assets/Layout/logo.png"
-                    width={500}
-                    height={60}
-                    alt="Logo"
-                    className={styles.logo}
-                    style={{marginLeft:!isLoggedIn?"0":"63px"}}
-                    onClick={()=>{router.push("/home")}}
+                        src="/assets/Layout/logo.png"
+                        width={500}
+                        height={60}
+                        alt="Logo"
+                        className={styles.logo}
+                        style={{ marginLeft: !isLoggedIn ? "0" : "63px" }}
+                        onClick={() => { router.push("/home") }}
                     />
-                    <div className={styles.logout} onClick={handleLogout}>LOGOUT</div>
+                    {isLoggedIn &&
+                        <div className={styles.logout} onClick={handleLogout}>LOGOUT</div>
+                    }
                 </div>
                 <div className={styles.navsubheader}></div>
             </div>
